9c90fafc152541c38d2113a15507f395dbf668e5
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-01-09 21:26:50 +0330

    add food model

--- resturant/food/models.py
+++ resturant/food/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=128,
+    )
+
+
+class Food(models.Model):
+    name = models.CharField(
+        max_length=256,
+    )
+    picture = models.ImageField(
+        upload_to='food/images/',
+        blank=False,
+        null=False,
+    )
+
+    price = models.PositiveBigIntegerField()
+    off_percent = models.PositiveSmallIntegerField(
+        default=0,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100),
+        ],
+    )
+
+    views = models.PositiveIntegerField(default=0)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
