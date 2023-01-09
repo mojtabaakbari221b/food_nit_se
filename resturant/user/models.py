9a8ecf5778835219f6ff3d2dd5ef9a01ce4841a9
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-01-09 21:42:07 +0330

    add cart model

--- resturant/user/models.py
+++ resturant/user/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils.translation import gettext as _
 from django.contrib.auth.models import AbstractUser
+from resturant.food.models import Food
 from . import model_fields
 from .managers import UserManager
+
 
 class User(AbstractUser):
     objects = UserManager()
@@ -28,3 +30,18 @@ class User(AbstractUser):
 User._meta.get_field('username').editable = False
 User._meta.get_field('username').blank = True
 User._meta.get_field('username').null = True
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+
+    foods = models.ManyToManyField(
+        Food,
+    )
+
+    is_paid = models.BooleanField(
+        default=False,
+    )
