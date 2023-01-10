3332b928a7605ac62f7719b382e6283bd3d8ee81
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-01-10 15:08:08 +0330

    config callback url in cart

--- resturant/cart/urls.py
+++ resturant/cart/urls.py
@@ -1,7 +1,14 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .api import viewsets
+from .views import PayCallback
+
+app_name = "cart"
 
 router = DefaultRouter()
-router.register('', viewsets.CartViewset, basename='cart')
+router.register('api/cart', viewsets.CartViewset, basename='cart')
 
 urlpatterns = router.urls
+urlpatterns = urlpatterns + [
+    path('cart/pay/callback', PayCallback.as_view(), name='verify_transaction')
+]
