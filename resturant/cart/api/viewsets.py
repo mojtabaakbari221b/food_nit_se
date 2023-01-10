8e6924a6dca73a6040b007a4428174c83960d4e6
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-01-10 19:48:11 +0330

    add and remove food from cart api

--- resturant/cart/api/viewsets.py
+++ resturant/cart/api/viewsets.py
@@ -1,9 +1,12 @@
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import action
 from resturant.zarinpal_extention.services import start_transaction
+from resturant.food.models import Food
 from ..models import Cart
 from .serializers import CartSerializer
 
@@ -27,6 +30,40 @@ class CartViewset(viewsets.GenericViewSet):
         serializer = self.serializer_class(cart, many=False)
 
         return Response(serializer.data)
+    
+    @action(detail=False , methods=["get"], url_path=r'add_food/(?P<food_id>[\w-]+)')
+    def add_food(self, request, food_id):
+        user = request.user
+        cart = user.get_my_cart()
+
+        food = get_object_or_404(
+            Food.objects.filter(
+                id=food_id,
+            )
+        )
+
+        cart.add_food_to_cart(
+            food,
+        )
+
+        return Response(status=status.HTTP_201_CREATED)
+    
+    @action(detail=False , methods=["get"], url_path=r'remove_food/(?P<food_id>[\w-]+)')
+    def remove_food(self, request, food_id):
+        user = request.user
+        cart = user.get_my_cart()
+
+        food = get_object_or_404(
+            Food.objects.filter(
+                id=food_id,
+            )
+        )
+
+        cart.remove_food_from_cart(
+            food,
+        )
+
+        return Response(status=status.HTTP_201_CREATED)
     
     @action(detail=False , methods=["get"])
     def pay(self, request):
