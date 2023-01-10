93588c87a1bee8519cb74f9922cf7583c77f6eff
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-01-10 09:31:55 +0330

    prefetch related on get cart duo to prevent n+1 problem

--- resturant/cart/managers.py
+++ resturant/cart/managers.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import BaseUserManager
 
 class FoodManager(BaseUserManager):
     def get_my_cart(self, user):
-        query = self.get_or_create(
+        query = self.all().prefetch_related('foods__category').get_or_create(
             is_paid=False,
             user=user,
         )
