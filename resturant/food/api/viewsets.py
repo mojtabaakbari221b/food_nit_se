4aa2f66c1334c22138a62d9a68f607951746812c
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-01-10 19:49:01 +0330

    filtering and ordering in food viewset

--- resturant/food/api/viewsets.py
+++ resturant/food/api/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework import filters
+from url_filter.integrations.drf import DjangoFilterBackend
 from ..models import Food
 from .serializers import FoodSerializer
 
@@ -6,3 +8,20 @@ from .serializers import FoodSerializer
 class FoodViewset(viewsets.ReadOnlyModelViewSet):
     serializer_class = FoodSerializer
     queryset = Food.objects.all().select_related('category')
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+    ]
+
+    filter_fields = [
+        'name',
+        'location',
+        'category',
+    ]
+
+    ordering_fields = [
+        'views',
+        'off_percent',
+        'price',
+    ]
