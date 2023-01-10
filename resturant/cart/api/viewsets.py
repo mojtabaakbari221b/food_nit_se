9584c74d1eeea68f616df94a9e34d592dd085995
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-01-10 15:08:43 +0330

    pay cart viewset

--- resturant/cart/api/viewsets.py
+++ resturant/cart/api/viewsets.py
@@ -1,11 +1,14 @@
+from django.shortcuts import redirect
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.decorators import action
+from resturant.zarinpal_extention.services import start_transaction
 from ..models import Cart
 from .serializers import CartSerializer
 
 
-class CartViewset(viewsets.ReadOnlyModelViewSet):
+class CartViewset(viewsets.GenericViewSet):
     serializer_class = CartSerializer
 
     permission_classes = [
@@ -22,5 +25,23 @@ class CartViewset(viewsets.ReadOnlyModelViewSet):
         cart = user.get_my_cart()
 
         serializer = self.serializer_class(cart, many=False)
-        
+
         return Response(serializer.data)
+    
+    @action(detail=False , methods=["get"])
+    def pay(self, request):
+        user = request.user
+        cart = user.get_my_cart()
+
+        result = start_transaction(
+            request,
+            {
+                "user": user,
+                "amount": cart.get_price_of_items(),
+                "description": "welcome to our shop",
+                "mobile": "09111111111",
+                "email": user.email,
+            }
+        )
+
+        return redirect(result)
