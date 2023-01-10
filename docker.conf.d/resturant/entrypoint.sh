d8a3be21f60af1d709c71f1a9b2cfb6fdb21f334
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-01-10 21:18:27 +0330

    change runner server port

--- docker.conf.d/resturant/entrypoint.sh
+++ docker.conf.d/resturant/entrypoint.sh
@@ -1,3 +1,3 @@
 python ./manage.py collectstatic --no-input
 python ./manage.py migrate
-gunicorn configs.wsgi:application --bind 0.0.0.0:8010 --workers 3
+gunicorn configs.wsgi:application --bind 0.0.0.0:8013 --workers 3
