2db8b5e5cb13903f7ca7af0174ca3bcefe583483
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-01-10 09:32:55 +0330

    show serialized category in food serializer

--- resturant/food/api/serializers.py
+++ resturant/food/api/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
+from ..models import Food, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            'name',
+        ]
 
 
 class FoodSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(many=False)
+
+
     class Meta:
+        model = Food
         fields = [
             'name',
             'price',
