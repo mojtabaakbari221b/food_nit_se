d9832d847aad95abdb345d4bf94c6d87c55f6a80
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-01-20 15:11:01 +0330

    add static and media url

--- configs/urls.py
+++ configs/urls.py
@@ -30,6 +30,15 @@ urlpatterns = [
     path('api/token/', CustomObtainAuthToken.as_view())
 ]
 
+# media and static
+
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 # drf yasg doc
 
